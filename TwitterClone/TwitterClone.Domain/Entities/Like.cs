b4108749f59121bdb90bd73b82c2713846234f57
mdabunayem33 <mdabunayem33@gmail.com>
2026-08-14 15:30:31 +0600

    Add Like Class

---
+++ TwitterClone/TwitterClone.Domain/Entities/Like.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterClone.Domain.Entities
+{
+    internal class Like
+    {
+    }
+}
