e7c1fadafcd025e97314c7020fad683136a2bc3d
mdabunayem33 <mdabunayem33@gmail.com>
2026-08-14 16:25:18 +0600

    Add Message Class

--- TwitterClone/TwitterClone.Domain/Entities/Notification.cs
+++ TwitterClone/TwitterClone.Domain/Entities/Notification.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TwitterClone.Domain.Entities
+﻿namespace TwitterClone.Domain.Entities
 {
-    internal class Notification
+    public class Notification
     {
+        private Guid _id;
+        private Guid _userId;
+        private string _message = string.Empty;
+        private DateTime _createdat;
+        private bool _isRead;
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public Guid UserId
+        {
+            get { return _userId; }
+        }
+
+        public string Message
+        {
+            get { return _message; }
+        }
+
+        public DateTime CreatedAt
+        {
+            get { return _createdat; }
+        }
+
+        public bool IsRead
+        {
+            get { return _isRead; }
+            set { _isRead = value; }
+        }
     }
 }
