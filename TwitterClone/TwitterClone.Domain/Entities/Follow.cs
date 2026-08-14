cf70c7bf97ead503cbad2119ce14a26ed9582f0b
mdabunayem33 <mdabunayem33@gmail.com>
2026-08-14 16:12:51 +0600

    Add Follow Class

---
+++ TwitterClone/TwitterClone.Domain/Entities/Follow.cs
@@ -0,0 +1,27 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Follow
+    {
+        private Guid _followerid;
+        private Guid _followingid;
+        private DateTime _followat;
+
+        public Guid FollowerId
+        {
+            get { return _followerid; }
+        }
+
+        public Guid FollowingId
+        {
+            get { return _followingid; }
+        }
+
+        public DateTime FollowAt
+        {
+            get { return _followat; }
+        }
+
+
+
+    }
+}
