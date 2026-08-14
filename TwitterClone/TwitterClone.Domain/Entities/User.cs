06d5418104a1958c01febed7282fdc53a41d5668
mdabunayem33 <mdabunayem33@gmail.com>
2026-08-14 15:10:31 +0600

    Add User Class

---
+++ TwitterClone/TwitterClone.Domain/Entities/User.cs
@@ -0,0 +1,38 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class User
+    {
+        private Guid _id;
+        private string _FirstName;
+        private string _LastName;
+        private string _Email;
+
+        public User() {
+            _id = Guid.NewGuid();
+        
+        }
+        public Guid Id
+        { 
+          get { return _id; } 
+        }
+
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set { _FirstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _LastName; }
+            set { _LastName = value; }
+        }
+
+        public string Email
+        {
+            get { return _Email; }
+            set { _Email = value; }
+        }
+
+    }
+}
